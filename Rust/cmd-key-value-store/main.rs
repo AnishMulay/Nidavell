778d8bbb9c412793f2830fcc0da86017fe754011
AnishMulay <anishm7030@gmail.com>
2025-11-09 15:43:12 -0500

    Practised basic struct creation and member functions

--- Rust/cmd-key-value-store/main.rs
+++ Rust/cmd-key-value-store/main.rs
@@ -1,3 +1,19 @@
+struct Person {
+    name: String,
+    _age: u32,
+}
+
+impl Person {
+    fn new(name: String, _age: u32) -> Self {
+        Self {name, _age}
+    }
+
+    fn greet(&self) {
+        println!("Hello my name is {}", self.name);
+    }
+}
+
 fn main () {
-    println!("Hello, World!")
+    let anish = Person::new(String::from("Anish"), 25);
+    anish.greet();
 }
