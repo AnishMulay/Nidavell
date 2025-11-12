d7e861a5aed6b7b3ca608c1c286a2612ff0e661e
AnishMulay <anishm7030@gmail.com>
2025-11-12 13:04:50 -0500

    Added a log file to KVStore

--- Rust/cmd-key-value-store/kv.rs
+++ Rust/cmd-key-value-store/kv.rs
@@ -1,34 +1,40 @@
 use std::collections::HashMap;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 pub struct KVStore {
-    mp: HashMap<String, String>
+    mp: HashMap<String, String>,
+    log_file: File
 }
 
 impl KVStore {
     pub fn new() -> Self {
+        let Ok(lf) = OpenOptions::new().create(true).write(true).open("./kvstore/log") else {todo!()};
+        println!("successfully opened log file");
         Self {
             mp: HashMap::new(),
+            log_file: lf,
         }
     }
 
     pub fn run_command(&mut self, command: &String) {
         let words: Vec<&str> = command.trim().split_whitespace().collect();
-        let mut operation: &str = "";
 
         // possible optimization here, look into production level code to understand how its done there
         match words.get(0).copied() {
             Some("set") => {
-                let Some(keyString) = words.get(1).copied() else {todo!()};
-                let Some(valString) = words.get(2).copied() else {todo!()};
-                self.add(String::from(keyString), String::from(valString));
+                let Some(key_string) = words.get(1).copied() else {todo!()};
+                let Some(val_string) = words.get(2).copied() else {todo!()};
+                self.add(String::from(key_string), String::from(val_string));
             },
             Some("get") => {
-                let Some(keyString) = words.get(1).copied() else {todo!()};
-                self.get(String::from(keyString));
+                let Some(key_string) = words.get(1).copied() else {todo!()};
+                self.get(String::from(key_string));
             }
             Some("delete") => {
-                let Some(keyString) = words.get(1).copied() else {todo!()};
-                self.delete(String::from(keyString));
+                let Some(key_string) = words.get(1).copied() else {todo!()};
+                self.delete(String::from(key_string));
             },
             _ => {
                 println!("Invalid operation")
@@ -40,6 +46,7 @@ impl KVStore {
 
     pub fn add(&mut self, key: String, val: String) {
         println!("key {} added", key);
+        writeln!(self.log_file, "add {} {}", key, val);
         self.mp.insert(key, val);
     }
 
@@ -54,6 +61,7 @@ impl KVStore {
 
     pub fn delete(&mut self, key: String) {
         self.mp.remove(&key);
+        writeln!(self.log_file, "delete {}", key);
         println!("key {} deleted", key)
     }
 }
